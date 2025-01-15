18d9f0c4ec68e98e53b67064b5c1de4b014476e4
vishalsharma <outreach.vishal@gmail.com>
2025-01-15 13:55:01 +0530

    added class cpp file and explain about class

---
+++ Class/class.cpp
@@ -0,0 +1,21 @@
+// It is a usser defined data-type and a blue print for creating objects
+
+// Important Point:- Now by default the data member is private but ther are other ways to access it by writing functin inside class 
+// i.e by getter and setter functions
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student{
+
+    string name;
+    int age, roll_number;
+    string grade;
+
+};
+
+int main(){
+    //here student is data type and s1 is object or variable
+    
+
+}

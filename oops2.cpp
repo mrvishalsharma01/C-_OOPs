4b630820b56102c53f30a0434a5336d95404a4ab
vishalsharma <outreach.vishal@gmail.com>
2025-01-15 13:37:27 +0530

    made a new file for oops2 were i explained about class

---
+++ oops2.cpp
@@ -0,0 +1,14 @@
+//To avoid all this we want to make a class of name student where we can create our own defined user data type and can be used in our 
+//entire program as per our need 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student{
+
+    string name;
+    int age, roll_number;
+    string grade;
+
+};
+

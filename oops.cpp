603f9bca981eb68a6b2532bca0fced9231ecb105
vishalsharma <outreach.vishal@gmail.com>
2025-01-15 13:33:03 +0530

    added oops file

---
+++ oops.cpp
@@ -0,0 +1,24 @@
+// C++ OOPs
+// It is a approach or a programming pattern where the programs are strucutred around object rather than function and logic.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string name;
+    int age, roll_number;
+    string grade;
+
+//Now if we want to store the value of another person then we need to again declare  other name and age and rollnumber
+
+   cin>>name>>age>>roll_number>>grade;
+
+    string name1;
+    int age1, roll_number1;
+    string grade1;
+
+// Now if we take cin value of every data type it can be done and we can  print the above as per our requirement 
+
+//but what if we have we want to store 100 student or 1000 or others then what we shoudl we do we can create class
+
+}
